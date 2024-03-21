d2a339fa3d59fabc6f4a7381b16dfb5d181a2881
Naol Arega <naol.dev@gmail.com>
2024-03-21 22:52:10 +0300

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -6,7 +6,23 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let mut start_line = [u8::default(); 1024];
+                stream.read(&mut start_line).unwrap();
+
+                let mut start_line_sections = start_line.split(|byte| byte == &(b' '));
+
+                if let Some(_method) = start_line_sections.next() {
+                    /* Handle http verb */
+                }
+
+                if let Some(path) = start_line_sections.next() {
+                    if path == b"/" {
+                        stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                    } else {
+                        stream.write(b"HTTP/1.1 400 Not Found\r\n\r\n").unwrap();
+                    }
+                }
+
                 stream.flush().unwrap();
             }
             Err(e) => {
