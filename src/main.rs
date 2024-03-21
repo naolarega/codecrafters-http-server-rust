8b90ffbc87fc783720cb2f6aa6863c13e8e98040
Naol Arega <naol.dev@gmail.com>
2024-03-21 23:41:05 +0300

    feat: reverted back `/`

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,31 @@ fn main() {
                     /* Handle http verb */
                 }
 
-                if let Some(path) = start_line_sections.next() {
-                    let mut path_sections = path.split(|byte| byte == &(b'/'));
+                match start_line_sections.next() {
+                    Some(b"/") => { stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(); },
+                    Some(path) => {
+                        let mut path_sections = path.split(|byte| byte == &(b'/'));
 
-                    // First empty section
-                    path_sections.next().unwrap();
+                        // First empty section
+                        path_sections.next().unwrap();
 
-                    let mut response_body = None;
+                        let mut response_body = None;
 
-                    if let Some(b"echo") = path_sections.next() {
-                        if let Some(random_string) = path_sections.next() {
-                            response_body = Some(random_string);
+                        if let Some(b"echo") = path_sections.next() {
+                            if let Some(random_string) = path_sections.next() {
+                                response_body = Some(random_string);
+                            }
                         }
-                    }
-                    
-                    if let Some(body) = response_body {
-                        stream.write(b"HTTP/1.1 200 OK\r\n").unwrap();
-                        stream.write(b"Content-Type: text/plain\r\n\r\n").unwrap();
-                        stream.write(body).unwrap();
-                    } else {
-                        stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
-                    }
+                        
+                        if let Some(body) = response_body {
+                            stream.write(b"HTTP/1.1 200 OK\r\n").unwrap();
+                            stream.write(b"Content-Type: text/plain\r\n\r\n").unwrap();
+                            stream.write(body).unwrap();
+                        } else {
+                            stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                        }
+                    },
+                    _ => return
                 }
 
                 stream.flush().unwrap();
