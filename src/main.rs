07c84512e862884bd441859d7c5e3ebc19bfc243
Naol Arega <naol.dev@gmail.com>
2024-03-21 22:22:56 +0300

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                stream.flush().unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
